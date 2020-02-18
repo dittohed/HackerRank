719c21edf3772dc471a55da6205c827ffcb33822
dittohed <wojtek.konieczkowicz@gmail.com>
2020-02-18 17:40:17 +0100

    .

--- bot_clean.py
+++ bot_clean.py
@@ -5,50 +5,28 @@ b---d
 --d--
 ----d
 
-strategy: find the nearest one and go for it
+greedy strategy: find the nearest one and go for it
 """
 
-#!/usr/bin/python
-
-# Head ends here
-
 def next_move(posr, posc, board):
-    calc_distances(posr, posc, board) #not the most efficient, but cheap enough
-    b_r, b_c = closest(posr, posc, board)
+    min = 8 #max initial distance
+
+    for (i, j) in [(x, y) for x in range(5) for y in range(5)]:
+            if board[i][j] == 'd':
+                distance = abs(posr - i) + abs(posc - j)
+                if distance < min:
+                    b_r, b_c = i, j
+                    min = distance
 
     if b_r == posr and b_c == posc:
-        return "CLEAN"
-        board[b_r][b_c] = '-'
+        print("CLEAN")
     else:
         diff_r, diff_c = b_r - posr, b_c - posc
         if diff_r > 0:
-            return "DOWN"
+            print("DOWN")
         elif diff_r < 0:
-            return "UP"
+            print("UP")
         elif diff_c > 0:
-            return "RIGHT"
+            print("RIGHT")
         elif diff_c < 0:
-            return "LEFT"
-
-def calc_distances(posr, posc, board): #fills board with distances instead of b chars
-    for (i, j) in [(x, y) for y in range(5) for x in range(5)]:
-        if board[i][j] == 'b':
-            distance = abs(posr - i) + abs(posc - j)
-            board[i][j] = distance
-
-def closest(posr, posc, board): #returns pos of the closest b
-    min = 8 #max initial distance
-
-    for (i, j) in [(x, y) for y in range(5) for x in range(5)]:
-        if board[i][j].is_integer():
-            if board[i][j] < min:
-                min_r, min_c = i, j
-
-    return min_r, min_c
-
-# Tail starts here
-
-if __name__ == "__main__":
-    pos = [int(i) for i in input().strip().split()]
-    board = [[j for j in input().strip()] for i in range(5)]
-    next_move(pos[0], pos[1], board)
+            print("LEFT")
---
+++ test.py
@@ -0,0 +1,6 @@
+def my_print():
+    print(a)
+
+a = 5
+my_print()
+#czyli a jest zmienną globalną?
